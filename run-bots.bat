5d4c632659dca3a19c7fe402bed9456f94146487
Ahmad Rafi Maliki <rafimaliki28@gmail.com>
2024-03-09 17:28:50 +0700

    fix folder structure

--- run-bots.bat
+++
@@ -1,5 +0,0 @@
-@echo off 
-start cmd /c "python main.py --logic SulfatGemoy --email=SG1@email.com --name=SG1 --password=123456 --team etimo --board 5"
-start cmd /c "python main.py --logic SulfatGemoy --email=SG2@email.com --name=SG2 --password=123456 --team etimo --board 5"
-start cmd /c "python main.py --logic SulfatGemoy --email=SG3@email.com --name=SG3 --password=123456 --team etimo --board 5"
-start cmd /c "python main.py --logic SulfatGemoy --email=SG4@email.com --name=SG4 --password=123456 --team etimo --board 5"
---
+++ src/run-bots.bat
@@ -0,0 +1,2 @@
+@echo off 
+start cmd /c "python main.py --logic SulfatGemoy --email=SulfatGemoy@email.com --name=SulfatGemoy --password=123456 --team etimo --board 1"
